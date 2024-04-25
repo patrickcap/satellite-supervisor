c409a7275f6f9ac16d37a0561b72c7fba25380e5
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-04-25 20:07:19 +0100

    api working with dummy result

--- src/api/operations/create_prediction.py
+++ src/api/operations/create_prediction.py
@@ -7,7 +7,8 @@ from fastapi import APIRouter, Depends, FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.security import APIKeyHeader
 
-from src.api.errors import UnauthorizedError, new_error_response
+from src.api.errors.errors import UnauthorizedError
+from src.api.errors.error_response import new_error_response
 from src.api.resources.satellite import Satellite
 from src.api.resources.post_satellite_purpose_response import PostSatellitePurposeResponse
 from src.model.model import Model
@@ -28,13 +29,13 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:  # pylint: disab
 
 
 @prediction_router.post('', response_model=PostSatellitePurposeResponse, status_code=201)
-async def predict_air_quality(satellite: Satellite, api_key: str = Depends(APIKeyHeader(name='X-API-key'))) -> PostSatellitePurposeResponse | JSONResponse:  # pylint: disable=line-too-long
+async def predict_satellite_purpose(satellite: Satellite, api_key: str = Depends(APIKeyHeader(name='X-API-key'))) -> PostSatellitePurposeResponse | JSONResponse:  # pylint: disable=line-too-long
     if api_key != os.getenv('API_KEY'):
         return JSONResponse(
             content=new_error_response([UnauthorizedError()], status=UnauthorizedError.status_code),
             status_code=UnauthorizedError.status_code
         )
-    return PostSatellitePurposeResponse(air_quality=_get_satellite_purpose_prediction(satellite))
+    return PostSatellitePurposeResponse(satellite_purpose=_get_satellite_purpose_prediction(satellite))
 
 
 def _get_satellite_purpose_prediction(satellite: Satellite) -> float:
